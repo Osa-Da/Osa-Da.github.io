7baae89f04751a310fc9594cb7aaa2ed710d5f43
sa.osincev <sa.osincev@tensor.ru>
2025-12-19 14:14:48 +0500

    пупупу - типо навигация

---
+++ apps.js
@@ -0,0 +1,27 @@
+// id: идентификатор {число},
+// name: название {строка},
+// url: url {строка},
+// description: описание {строка},
+// password: пароль {строка}
+
+const APPS = [
+    {
+        id: 'calculator',
+        url: 'apps/alcoCalc.html',
+        title: 'Калькулятор разбодяживания',
+        icon: 'fas fa-calculator',
+        description: 'Продвинутый калькулятор с научными функциями',
+    },
+    {
+        id: 'notes',
+        url: 'apps/questing.html',
+        title: 'Новогодние вопросы',
+        icon: 'fas fa-sticky-note',
+        description: 'Захади и отвечай на вопросы ',
+    },
+    {
+        id: 'weather',
+        url: 'apps/bigFive.html',
+        title: 'Тест личности',
+    },
+];
